250b47f958706295bb16a69bdde792e7bf318df0
Wayne Yeh <wayne.yeh@eten.com.tw>
2017-09-29 13:58:19 +0800

    Initial Commit

---
+++ strings2storyboard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  strings2storyboard
+//
+//  Created by Wayne Yeh on 2017/9/29.
+//  Copyright © 2017年 Wayne Yeh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
